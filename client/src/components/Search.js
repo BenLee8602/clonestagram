d55c3fe6691e6ffbb3d6b0c0ee3e8810a39e55cb
ben <ben.lee8602@gmail.com>
2023-06-10 22:00:51 -0400

    separate user and post searches

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -4,58 +4,76 @@ import UserList from "./UserList";
 
 import "../style/Search.css";
 
+
 function Search() {
     const [input, setInput] = useState("");
-    const [query, setQuery] = useState("");
-    const [users, setUsers] = useState([]);
-    const [posts, setPosts] = useState([]);
-    const [show, setShow] = useState("users");
 
-    const handleSearch = () => {
+    const [userQuery, setUserQuery] = useState("");
+    const [userResults, setUserResults] = useState([]);
+
+    const [postQuery, setPostQuery] = useState("");
+    const [postResults, setPostResults] = useState([]);
+
+    const [view, setView] = useState("users");
+
+
+    const handleUserSearch = () => {
         if (input === "") return;
-        setQuery(input);
-        
+        setUserQuery(input);
         fetch(`${process.env.REACT_APP_BACKEND_API}/users/search/${input}`)
         .then(res => res.json().then(body => ({ status: res.status, body })))
-        .then(res => res.status === 200 ? setUsers(res.body) : console.log(res.body));
-
-        fetch(`${process.env.REACT_APP_BACKEND_API}/posts/search/${input}`)
-        .then(res => res.json().then(body => ({ status: res.status, body })))
-        .then(res => res.status === 200 ? setPosts(res.body) : console.log(res.body));
+        .then(res => res.status === 200 ? setUserResults(res.body) : console.log(res.body));
     };
 
-    return (<>
-        <div className="search">
+    const handlePostSearch = () => {
+        if (input === "") return;
+        setPostQuery(input);
+        fetch(`${process.env.REACT_APP_BACKEND_API}/posts/search/${input}`)
+        .then(res => res.json().then(body => ({ status: res.status, body })))
+        .then(res => res.status === 200 ? setPostResults(res.body) : console.log(res.body));
+    };
+
+    const handleSearch = () => view === "users" ? handleUserSearch() : handlePostSearch();
+
+
+    return (<div className="search">
+        <button
+            onClick={ () => setView("users") }
+            className="search-switch"
+            id={ view === "users" ? "search-switch-active" : "" }
+        >users</button>
+        <button
+            onClick={ () => setView("posts") }
+            className="search-switch search-switch-right"
+            id={ view === "posts" ? "search-switch-active" : "" }
+        >posts</button>
+
+        <div className="search-body">
             <input
                 type="text"
-                placeholder="search users and posts"
+                placeholder={`search ${view}`}
                 onChange={ e => setInput(e.target.value) }
                 className="search-bar"
             />
             <button onClick={ handleSearch } className="search-button">search</button>
-        </div>
-        
-        { query ? <>
-            <div className="search">
-                <button
-                    onClick={ () => setShow("users") }
-                    className="search-switch"
-                    id={ show === "users" ? "search-switch-active" : "" }
-                >users</button>
-                <button
-                    onClick={ () => setShow("posts") }
-                    className="search-switch"
-                    id={ show === "posts" ? "search-switch-active" : "" }
-                >posts</button>
-            </div>
 
-            { show === "users" ? (
-                users.length ? <UserList data={ users } /> : <h3>No users found for "{ query }"</h3>
-            ) : (
-                posts.length ? <PostList posts={ posts } /> : <h3>No posts found for "{ query }"</h3>
-            ) }
-        </> : <></> }
-    </>);
+            { userQuery && view === "users" ? (
+                userResults.length ? (
+                    <UserList data={ userResults } />
+                ) : (
+                    <p className="no-results">No users found for "{ userQuery }"</p>
+                )
+            ) : <></> }
+            { postQuery && view === "posts" ? (
+                postResults.length ? (
+                    <PostList posts={ postResults } />
+                ) : (
+                    <p className="no-results">No posts found for "{ postQuery }"</p>
+                )
+            ) : <></> }
+        </div>
+    </div>);
 }
+
 
 export default Search;
