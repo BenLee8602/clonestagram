3385c9740cf288847da7a9f315fc8bef7a525b0b
ben <ben.lee8602@gmail.com>
2023-07-18 14:34:06 -0400

    add biglist component

---
+++ client/src/components/BigList.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useRef, useState } from "react";
+
+
+function BigList({ route, req = undefined, map, container = null }) {
+    const [data, setData] = useState([]);
+    const [good, setGood] = useState(true);
+    const [page, setPage] = useState(0);
+    const [start, setStart] = useState(new Date().getTime());
+
+    const pageRef = useRef(page);
+    const goodRef = useRef(good);
+    const bottom = useRef(null);
+
+    useEffect(() => pageRef.current = page, [page]);
+    useEffect(() => goodRef.current = good, [good]);
+
+
+    const loadMore = async () => {
+        if (!goodRef.current) return;
+        setGood(false);
+        try {
+            const url = `${process.env.REACT_APP_BACKEND_API}/${route}?page=${pageRef.current}&start=${start}`;
+            console.log(url);
+            const res = await fetch(url, req);
+            const body = await res.json();
+            if (res.status !== 200 || body.length === 0) return setGood(false);
+            setPage(prev => prev + 1);
+            setData(prev => [...prev, ...body]);
+        } catch (err) { console.log(err); }
+        setGood(true);
+    };
+
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            for (const entry of entries) {
+                if (entry.isIntersecting) loadMore();
+            }
+        }, {
+            root: container,
+            rootMargin: "0px",
+            threshold: 0.1
+        });
+
+        if (bottom.current) observer.observe(bottom.current);
+        return () => {
+            if (bottom.current) observer.unobserve(bottom.current);
+        };
+    }, []);
+
+
+    return (<>
+        { data.map(map) }
+        <div ref={bottom} style={{"height": "32px"}}></div>
+    </>);
+}
+
+
+export default BigList;
